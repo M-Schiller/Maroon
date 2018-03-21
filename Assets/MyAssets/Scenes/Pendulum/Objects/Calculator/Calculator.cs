f3c50d2d140112401e4e617acf2fdd192fb352d4
wolfjuli <julian.wolf@student.tugraz.at>
2018-03-21 19:20:03 +0100

    Damn you intellij, because of you autosave is a standard functionality

--- Assets/MyAssets/Scenes/Pendulum/Objects/Calculator/Calculator.cs
+++ Assets/MyAssets/Scenes/Pendulum/Objects/Calculator/Calculator.cs
@@ -73,7 +73,7 @@ public class Calculator : MonoBehaviour {
     {
         double res;
         if (!double.TryParse(s, out res))
-            throw new Exception(string.Format("Calculator: internal error. Could not parse '{0} to double.", s));
+            throw new Exception(string.Format("Calculator: internal error. Could not parse '{0}' to double.", s));
         return res;
     }
 
